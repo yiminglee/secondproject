88bea4fa7acaabc9198aaee79786bf0229fb203b
彭璿哲 <jasonben26@gmail.com>
2019-05-27 11:36:23 +0800

    第3次commit
    修改Servlet1.java

--- LabWebGit/src/view/Servlet1.java
+++ LabWebGit/src/view/Servlet1.java
@@ -16,6 +16,7 @@ public class Servlet1 extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h3>Servlet1:"+time+"</h3>");
+		out.println("<h1>commit3</h1>");
 		out.close();
 	}
 
