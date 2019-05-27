478ffe497ed91bd57fd49440630a5ca21a9129cb
劉長軒 <henryleo200333@gmail.com>
2019-05-27 11:48:07 +0800

    Servlet1 Time Change
    Convert currentTimeMillis to Timestamp.

--- LabWebGit/src/view/Servlet1.java
+++ LabWebGit/src/view/Servlet1.java
@@ -2,6 +2,7 @@ package view;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,7 @@ public class Servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		long time = System.currentTimeMillis();
+		Timestamp time = new Timestamp(System.currentTimeMillis());
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h3>Servlet1:"+time+"</h3>");
