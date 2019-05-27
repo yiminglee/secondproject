e0e18aeaf7d89bf6c1e396b0a51c431de1d8bc29
劉長軒 <henryleo200333@gmail.com>
2019-05-27 11:50:47 +0800

    Servlet1 add Text
    Happy New Year.

--- LabWebGit/src/view/Servlet1.java
+++ LabWebGit/src/view/Servlet1.java
@@ -17,6 +17,7 @@ public class Servlet1 extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h3>Servlet1:"+time+"</h3>");
+		out.println("<h3 style='color:red;'>Happy New Year!</h3>");
 		out.close();
 	}
 
